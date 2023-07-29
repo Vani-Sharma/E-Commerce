7e517f79ce0720eef6ade185d4fda2cc9201aece
Vani Sharma <vanisharma826@gmail.com>
2023-07-29 17:25:04 +0530

    initiating cart and making routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./features/auth/components/Login";
+import Signup from "./features/auth/components/Signup";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+
+  {
+    path: "/cart",
+    element: <CartPage />,
+  },
+
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }

f48bd925ba46cc86e95f5c5b7ff423d44b0ca1b2
Vani Sharma <vanisharma826@gmail.com>
2023-08-27 01:20:01 +0530

    admin panel initiated

---
+++ src/features/auth/components/ProtectedAdmin.js
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { selectLoggedInUser } from "../authSlice";
+import { Navigate } from "react-router-dom";
+
+function ProtectedAdmin({ children }) {
+  const user = useSelector(selectLoggedInUser);
+  if (user && user.role == "user") {
+    return <Navigate to="/" required={true}></Navigate>;
+  }
+
+  return children;
+}
+
+export default Protected;
--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -38,6 +38,7 @@ function Signup() {
                   email: data.email,
                   password: data.password,
                   addresses: [],
+                  role: "user",
                 })
               );
               console.log(data);
